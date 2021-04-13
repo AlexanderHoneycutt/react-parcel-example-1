e41df5ede7013c2355feb885321a61a9523eb2fa
Alex Honeycutt <17honeyale@gmail.com>
2021-04-13 09:46:56 -0700

    April 13th Class Example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from "react";
-
-const viewHeight = 500;
-const viewWidth = 500;
+import WorldviewDemo from "./worldview-example";
 
 const App = () => {
-    return <svg style={{border: "1px solid pink", 
-                        width: viewWidth, height: viewHeight }}>
-                            <circle cx={20} cy={20} r="5"/>
+  return (
+    <div>
+      <p>from App rerender</p>
+      <p> now for a worldview demo! </p>
+      <p> class on April 13 </p>
+      <p> blue </p>
 
-                            <rect x={200} y={200} fill = "rgb(200, 200, 200)" width={10} height={10} />
-                            <rect x={212} y={200} fill = "rgb(200, 200, 200)" width={10} height={10} />
-                            <rect x={224} y={200} fill = "rgb(200, 200, 200)" width={10} height={10} />
-                            <rect x={236} y={200} fill = "rgb(200, 200, 200)" width={10} height={10} />
-                            <rect x={248} y={200} width={10} height={10} 
-                                fill = "black" />
-                            
-                            <line x1={0} y1={80} x2={150} y2={240} stroke="black" />
-                        
-                            <text x="20" y="35" class="small" style={{font: "italic 16px sans serif"}}>
-                                My dog is grumpy
-                            </text>
-                        </svg>;
+      <div style={{ width: "50vw", height: "50vh" }}>
+        <WorldviewDemo />
+      </div>
+    </div>
+  );
 };
 
 export default App;
