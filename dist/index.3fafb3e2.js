79b75921e9cb4365ce51f54763b65b78e4b3f3bf
Alex Honeycutt <17honeyale@gmail.com>
2021-04-20 10:02:08 -0700

    Render circles and lines

--- dist/index.3fafb3e2.js
+++ dist/index.3fafb3e2.js
@@ -26280,29 +26280,107 @@ try {
     _s();
     const [data, loading] = _hooksUseFetch.useFetch("https://raw.githubusercontent.com/AlexanderHoneycutt/react-parcel-example-1/main/data/country_vaccinations%20-%20Copy.csv");
     console.log("from hook", loading, data);
+    const chartSize = 500;
+    const margin = 20;
     return (
       /*#__PURE__*/_reactDefault.default.createElement("div", {
         __self: undefined,
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 15,
+          lineNumber: 18,
           columnNumber: 5
         }
       }, /*#__PURE__*/_reactDefault.default.createElement("h1", {
         __self: undefined,
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 16,
+          lineNumber: 19,
           columnNumber: 7
         }
       }, "Exploratory Data Analysis Assignment #2 Info 474, Spring 2021"), /*#__PURE__*/_reactDefault.default.createElement("p", {
         __self: undefined,
         __source: {
           fileName: _jsxFileName,
-          lineNumber: 17,
+          lineNumber: 20,
           columnNumber: 7
         }
-      }, loading && "Loading data!"))
+      }, loading && "Loading data!"), /*#__PURE__*/_reactDefault.default.createElement("h3", {
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 21,
+          columnNumber: 7
+        }
+      }, "Rendering Circles :) This shows a distribution of Daily Vaccinations"), /*#__PURE__*/_reactDefault.default.createElement("svg", {
+        width: chartSize,
+        height: chartSize,
+        style: {
+          border: "1px solid black"
+        },
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 22,
+          columnNumber: 7
+        }
+      }, data.slice(0, 300).map((measurement, index) => {
+        const highlight = measurement.country === "Afghanistan";
+        return (
+          /*#__PURE__*/_reactDefault.default.createElement("circle", {
+            key: index,
+            cx: highlight ? chartSize / 2 : chartSize / 2 - 20,
+            cy: chartSize - margin - measurement.daily_vaccinations,
+            r: "3",
+            fill: "none",
+            stroke: highlight ? "red" : "steelblue",
+            strokeOpacity: "0.2",
+            __self: undefined,
+            __source: {
+              fileName: _jsxFileName,
+              lineNumber: 26,
+              columnNumber: 13
+            }
+          })
+        );
+      })), /*#__PURE__*/_reactDefault.default.createElement("h3", {
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 39,
+          columnNumber: 7
+        }
+      }, "Rendering Lines :) This shows a distribution of Daily Vaccinations"), /*#__PURE__*/_reactDefault.default.createElement("svg", {
+        width: chartSize,
+        height: chartSize,
+        style: {
+          border: "1px solid black"
+        },
+        __self: undefined,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 41,
+          columnNumber: 7
+        }
+      }, data.slice(0, 300).map((measurement, index) => {
+        const highlight = measurement.country === "Afghanistan";
+        return (
+          /*#__PURE__*/_reactDefault.default.createElement("line", {
+            key: index,
+            x1: chartSize / 2,
+            y1: chartSize - margin - measurement.daily_vaccinations,
+            x2: chartSize / 2 + 20,
+            y2: chartSize - margin - measurement.daily_vaccinations,
+            stroke: highlight ? "red" : "steelblue",
+            strokeOpacity: highlight ? 1 : 0.1,
+            __self: undefined,
+            __source: {
+              fileName: _jsxFileName,
+              lineNumber: 45,
+              columnNumber: 13
+            }
+          })
+        );
+      })))
     );
   };
   _s(App, "Jm65JCcgUFoenM4DufkEA80vRVI=", false, function () {
--- src/App.js
+++ src/App.js
@@ -11,10 +11,50 @@ const App = () => {
 
   console.log("from hook", loading, data);
 
+  const chartSize = 500;
+  const margin = 20;
+
   return (
     <div>
       <h1>Exploratory Data Analysis Assignment #2 Info 474, Spring 2021</h1>
-      <p>{loading && "Loading data!"}</p> 
+      <p>{loading && "Loading data!"}</p>
+      <h3>Rendering Circles :) This shows a distribution of Daily Vaccinations</h3>
+      <svg width={chartSize} height={chartSize} style={{ border: "1px solid black" }}>
+        {data.slice(0, 300).map((measurement, index) => {
+          const highlight = measurement.country === "Afghanistan";
+          return (
+            <circle
+              key={index}
+              cx={highlight ? chartSize / 2 : chartSize / 2 - 20}
+              cy={chartSize - margin - measurement.daily_vaccinations}
+              r="3"
+              fill="none"
+              stroke={highlight ? "red" : "steelblue"}
+              strokeOpacity="0.2"
+            />
+          );
+        })}
+      </svg>
+
+      <h3>Rendering Lines :) This shows a distribution of Daily Vaccinations</h3>
+      
+      <svg width={chartSize} height={chartSize} style={{ border: "1px solid black" }}>
+        {data.slice(0, 300).map((measurement, index) => {
+          const highlight = measurement.country === "Afghanistan";
+          return (
+            <line
+              key={index}
+              x1={chartSize / 2}
+              y1={chartSize - margin - measurement.daily_vaccinations}
+              x2={chartSize / 2 + 20}
+              y2={chartSize - margin - measurement.daily_vaccinations}
+              stroke={highlight ? "red" : "steelblue"}
+              strokeOpacity={highlight ? 1 : 0.1}
+            />
+          );
+        })}
+      </svg>
+
     </div>
   );
 };
